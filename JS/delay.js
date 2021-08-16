f49dfdbd0704ca6885fd1bdb24518ebbb000f136
prem9141 <premkumar9141@gmail.com>
2021-08-16 11:24:15 -0400

    Timeout Function for Animation

--- JS/delay.js
+++ JS/delay.js
@@ -1,3 +1,4 @@
+/* Function to setTimeOut for animation effect */
 const delay = (first,second,isSort,count) =>  { return new Promise(resolve => {
     setTimeout (() => resolve(drawRect()),delaySec);
 }); };
